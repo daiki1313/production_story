d7f34b1d70ab5e29e7a9c97b05ce7e0e6c9ef643
daiki1313 <daityan13@gmail.com>
2024-09-13 15:12:30 +0900

    add api routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :following, only: %i[index]
+        resources :followers, only: %i[index]
+        resources :posts
+      end
+      resources :posts, only: %i[index]
+      resources :likes, only: %i[create destroy]
+      resources :relationships, only: %i[create, destroy]
+    end
+  end
 end
