6d3d944ca57e2d20e4dcd282d8ed403fd4f65cf0
daiki1313 <daityan13@gmail.com>
2024-09-17 15:51:08 +0900

    api add devise gem

---
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -0,0 +1,16 @@
+# アカウント作成用コントローラー
+module Api
+  module V1
+    module Auth
+
+      class RegistrationsController < DeviseTokenAuth::RegistrationsController
+        private
+
+          def sign_up_params
+            params.permit(:email, :password, :password_confirmation, :name)
+          end
+      end
+
+    end
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   namespace :api do
     namespace :v1 do
       resources :users do
@@ -6,9 +7,18 @@ Rails.application.routes.draw do
         resources :followers, only: %i[index]
         resources :posts
       end
+      resources :test, only: %i[index]
       resources :posts, only: %i[index]
       resources :likes, only: %i[create destroy]
       resources :relationships, only: %i[create, destroy]
+
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
     end
   end
 end
